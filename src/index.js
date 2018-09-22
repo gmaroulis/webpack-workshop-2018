0c8512f2bbafb3ea1d33ee5af7a87094c59e38a2
Georgios Maroulis <georgios.maroulis@gmail.com>
2018-09-22 14:38:27 +0300

    webpack debug examples

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import nav from './nav';
+import { top, bottom } from './footer';
+import makeButton from './button';
+import  { red, blue, makeColorStyle } from './button-styles'
+
+console.log(
+    nav(),
+    top,
+    bottom,
+    makeButton('MyFirstButton'),
+    makeColorStyle('cyan')
+);
